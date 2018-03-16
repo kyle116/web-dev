b119bc73fb9b716119c3106299cc8feae914dc42
Kyle Liu <kyle116@yahoo.com>
2018-03-16 15:59:45 -0700

    done number 5

--- week2/2_tuesday/dom-exercise/solution/main.js
+++ week2/2_tuesday/dom-exercise/solution/main.js
@@ -2,10 +2,12 @@
 document.getElementById("box-1").addEventListener("click", function() {
   document.getElementById("box-1").style.backgroundColor = "blue";
 })
+
 // Exercise 2 - Move your mouse over the box to change it to the color red
 document.getElementById("box-2").addEventListener("mouseover", function() {
   document.getElementById("box-2").style.backgroundColor = "red";
 })
+
 // Exercise 3 - Click each li to toggle it between Black color font and Red color font
 var liArr = document.getElementsByTagName("li");
 for (i = 0; i < liArr.length; i++) {
@@ -14,9 +16,18 @@ for (i = 0; i < liArr.length; i++) {
   })
 }
 
-// Exercise 4
-// Exercise 5
-// Exercise 6
+// Exercise 4 - Click the box to generate a new random number(between 1-100) inside the box
+document.getElementById("box-4").addEventListener("click", function() {
+  document.getElementById("box-4").innerHTML = Math.floor((Math.random() * 100) + 1);
+})
+// Exercise 5 - Click on the box to generate a new identical box(Same height, same width, same border). You should not type in the css file at all
+document.getElementById("box-5").addEventListener("click", function() {
+  var newDiv = document.createElement("div");
+  newDiv.className = "box";
+  document.getElementById("number-5").insertBefore(newDiv, document.getElementById("number-5").children[2]);
+})
+// Exercise 6 - By clicking any of the boxes, change the first box color to Yellow, the second to Purple, the third to Blue
+
 // Exercise 7
 document.getElementById("answer-7").innerHTML = document.title;
 // Exercise 8
